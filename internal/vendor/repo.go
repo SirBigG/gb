e0544262a72ebfaf3e927fd9938bf75ff6698bbc
Andrii Hots <andriihots@Andriis-MacBook-Pro.local>
2018-11-01 14:14:56 +0200

    Fix import

--- internal/vendor/repo.go
+++ internal/vendor/repo.go
@@ -12,7 +12,7 @@ import (
 	"regexp"
 	"strings"
 
-	"github.com/constabulary/gb/internal/fileutils"
+	"github.com/SirBigG/gb/internal/fileutils"
 )
 
 // RemoteRepo describes a remote dvcs repository.
